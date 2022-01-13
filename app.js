d032e40ccac5e0063927315cfd69e5813924e85c
racmathafidz <rac.hafidz@gmail.com>
2022-01-13 15:56:52 +0700

    [FEAT] Add CORS configuration

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', process.env.origin);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // User's session management
 app.use(session({
