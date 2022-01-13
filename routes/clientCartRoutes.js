0b59dcc3182f9f8420aa041de5c14437e25479fe
racmathafidz <rac.hafidz@gmail.com>
2022-01-13 13:07:09 +0700

    [FEAT] Add client cart route

---
+++ routes/clientCartRoutes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const cartControllers = require('../controllers/clientCartControllers');
+
+const router = Router();
+
+router.post('/post', cartControllers.post_cart);
+router.delete('/delete/:id', cartControllers.delete_cart);
+router.get('/:id', cartControllers.get_cart);
+
+module.exports = router;
