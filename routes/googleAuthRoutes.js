4ae43808ae49bc344b0cd78caf28ae01c0da6d77
racmathafidz <rac.hafidz@gmail.com>
2022-01-13 12:42:03 +0700

    [REFACTOR] Renaming file's name

--- routes/googleAuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const googleAuthControllers = require('../controllers/googleAuthControllers');
-
-const router = Router();
-
-router.post('/', googleAuthControllers.post_auth_google);
-
-module.exports = router;
