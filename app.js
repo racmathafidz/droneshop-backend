b678f372c49e06997e9fdfa1e518bf2d50d96146
racmathafidz <rac.hafidz@gmail.com>
2022-01-13 17:06:21 +0700

    [FIX] Fix cors Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', process.env.origin);
+  res.header('Access-Control-Allow-Origin', 'https://racx-droneshop.netlify.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
