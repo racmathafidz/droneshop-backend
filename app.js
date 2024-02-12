d4fdb5addcc21cb5a29bfde5046c301f47dad9f0
Racmat Hafidz Fadli <rac.hafidz@gmail.com>
2024-02-12 18:32:40 +0700

    update: new version package

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-const flash = require('connect-flash');
-require('dotenv').config();
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const flash = require("connect-flash");
+require("dotenv").config();
 
-const Product = require('./models/Product');
-const User = require('./models/User');
-const Admin = require('./models/Admin');
-const Cart = require('./models/Cart');
-const Transaction = require('./models/Transaction');
-const adminAuthRoutes = require('./routes/adminAuthRoutes');
-const clientGoogleAuthRoutes = require('./routes/clientGoogleAuthRoutes');
-const clientLocalAuthRoutes = require('./routes/clientLocalAuthRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const clientProductRoutes = require('./routes/clientProductRoutes');
-const clientCartRoutes = require('./routes/clientCartRoutes');
-const clientTransactionRoutes = require('./routes/clientTransactionRoutes');
-const requireClientAuth = require('./middleware/clientAuthMiddleware');
-const { requireAdminAuth, checkAdmin } = require('./middleware/adminAuthMiddleware');
+const Product = require("./models/Product");
+const User = require("./models/User");
+const Admin = require("./models/Admin");
+const Cart = require("./models/Cart");
+const Transaction = require("./models/Transaction");
+const adminAuthRoutes = require("./routes/adminAuthRoutes");
+const clientGoogleAuthRoutes = require("./routes/clientGoogleAuthRoutes");
+const clientLocalAuthRoutes = require("./routes/clientLocalAuthRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const clientProductRoutes = require("./routes/clientProductRoutes");
+const clientCartRoutes = require("./routes/clientCartRoutes");
+const clientTransactionRoutes = require("./routes/clientTransactionRoutes");
+const requireClientAuth = require("./middleware/clientAuthMiddleware");
+const {
+  requireAdminAuth,
+  checkAdmin,
+} = require("./middleware/adminAuthMiddleware");
 
 // Port
 const PORT = process.env.PORT || 5000;
@@ -29,33 +32,43 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json()); // Convert request body to json
 app.use(express.urlencoded({ extended: true })); // Convert request body and html post form-data to json
-app.use(express.static(path.join(__dirname, 'public'))); // Set static public folder
-app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2'))); // Set static sb-admin-2 node-modules for accesing css
+app.use(express.static(path.join(__dirname, "public"))); // Set static public folder
+app.use(
+  "/sb-admin-2",
+  express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
+); // Set static sb-admin-2 node-modules for accesing css
 
 // Views
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Cors
-app.use(cors({
-  origin: process.env.origin,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.origin,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'https://racx-droneshop.netlify.app');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", process.env.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // User's session management
-app.use(session({
-  secret: process.env.secretKey, // Cookie secret
-  resave: true,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 },
-}));
+app.use(
+  session({
+    secret: process.env.secretKey, // Cookie secret
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
 
 // Parsing cookie
 app.use(cookieParser(process.env.secretKey)); // Cookie secret, have to match with session's secret
@@ -64,7 +77,11 @@ app.use(cookieParser(process.env.secretKey)); // Cookie secret, have to match wi
 app.use(flash());
 
 // Connecting to database and start listening
-mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Connected to Database and Start Listening on Port ${PORT}`);
@@ -73,21 +90,21 @@ mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology:
   .catch((err) => console.log(err));
 
 // Routes
-app.get('/', (req, res) => {
-  res.redirect('/auth/admin');
+app.get("/", (req, res) => {
+  res.redirect("/auth/admin");
 });
-app.use('/admin', requireAdminAuth, checkAdmin, adminRoutes);
-app.use('/auth/google', clientGoogleAuthRoutes);
-app.use('/auth/local', clientLocalAuthRoutes);
-app.use('/auth/admin', adminAuthRoutes);
-app.use('/api/product', clientProductRoutes);
-app.use('/api/cart', requireClientAuth, clientCartRoutes);
-app.use('/api/transaction', requireClientAuth, clientTransactionRoutes);
+app.use("/admin", requireAdminAuth, checkAdmin, adminRoutes);
+app.use("/auth/google", clientGoogleAuthRoutes);
+app.use("/auth/local", clientLocalAuthRoutes);
+app.use("/auth/admin", adminAuthRoutes);
+app.use("/api/product", clientProductRoutes);
+app.use("/api/cart", requireClientAuth, clientCartRoutes);
+app.use("/api/transaction", requireClientAuth, clientTransactionRoutes);
 
 // 404 Page Not Found Route
-app.get('*', (req, res) => {
-  res.render('admin/404/view_404.ejs', {
-    title: 'Page Not Found',
+app.get("*", (req, res) => {
+  res.render("admin/404/view_404.ejs", {
+    title: "Page Not Found",
     userName: res.locals.user.userName,
   });
 });
--- controllers/clientGoogleAuthControllers.js
+++ controllers/clientGoogleAuthControllers.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken');
-const { OAuth2Client } = require('google-auth-library');
+const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
@@ -10,40 +10,39 @@ const post_auth_google = async (req, res) => {
 
   const ticket = await client.verifyIdToken({
     idToken: token,
-    audience: process.env.clientID,
+    audience: process.env.GOOGLE_CLIENT_ID,
   });
 
   const { name, email, picture } = ticket.getPayload();
 
-  User.findOne({ email })
-    .then(async (response) => {
-      if (response) {
-        // req.session.userId = response.id;
+  User.findOne({ email }).then(async (response) => {
+    if (response) {
+      // req.session.userId = response.id;
 
-        // Jwt expires time (1 day)
-        const maxAge = 1 * 24 * 60 * 60;
-        // Create jwt token
-        const jwtToken = jwt.sign({ id: response.id }, process.env.secretKey, {}); // No expired date
-        // const jwtToken = jwt.sign({ id: response.id }, process.env.secretKey, { expiresIn: maxAge }); // With expired date
+      // Jwt expires time (1 day)
+      const maxAge = 1 * 24 * 60 * 60;
+      // Create jwt token
+      const jwtToken = jwt.sign({ id: response.id }, process.env.secretKey, {}); // No expired date
+      // const jwtToken = jwt.sign({ id: response.id }, process.env.secretKey, { expiresIn: maxAge }); // With expired date
 
-        res.status(201).send({
-          id: response._id,
-          email: response.email,
-          fullName: response.fullName,
-          imageProfile: response.imageProfile,
-          token: jwtToken,
-        });
-      } else {
-        const user = await User.create({
-          email,
-          fullName: name,
-          imageProfile: picture,
-        });
+      res.status(201).send({
+        id: response._id,
+        email: response.email,
+        fullName: response.fullName,
+        imageProfile: response.imageProfile,
+        token: jwtToken,
+      });
+    } else {
+      const user = await User.create({
+        email,
+        fullName: name,
+        imageProfile: picture,
+      });
 
-        req.session.userId = user.id;
-        res.status(201).send(user);
-      }
-    });
+      req.session.userId = user.id;
+      res.status(201).send(user);
+    }
+  });
 };
 
 module.exports = {
